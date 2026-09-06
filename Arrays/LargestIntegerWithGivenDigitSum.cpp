9c744fceb765f88fc77541cbc1c51d0b75b41115
piousgupta582-coder <piousgupta582@gmail.com>
2026-09-06 21:14:14 +0530

    Implement largestInteger function for digit sum

--- Arrays/LargestIntegerWithGivenDigitSum.cpp
+++ Arrays/LargestIntegerWithGivenDigitSum.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int findMaxConsecutiveOnes(vector<int>& nums) {
-        int n = nums.size();
-        int a =0;
-        int cnt =0;
-        for(int i = 0;i<n;i++){
-            if(nums[i]==1){
-                a++;
-                cnt = max(cnt,a);
-            }
-            else{
-                a= 0;
-            }
-        }
-        return cnt;
+    int largestInteger(int n, int s) {
+      if(s>9*n) return -1;
+      int ans = 0;
+      for(int i =0; i<n;i++){
+        int digit = min(9,s);
+        ans = ans*10+digit;
+        s= s-digit;
+      }  
+        return ans;
     }
 };
